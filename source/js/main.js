06635e8d0bb811f04ed810e691e236ca74fd9ce3
Marina <marishkina@gmail.com>
2021-06-20 17:35:36 +0800

    добавляет страничку и анимацию

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {ieFix} from './utils/ie-fix';
 import {iosVhFix} from './utils/ios-vh-fix';
 
 import {initModals} from './modules/init-modals';
+import {initMainScreen} from './modules/init-main-screen';
 
 // Utils
 // ---------------------------------
@@ -13,3 +14,4 @@ iosVhFix();
 // ---------------------------------
 
 initModals();
+initMainScreen();
---
+++ source/js/modules/init-main-screen.js
@@ -0,0 +1,52 @@
+const initMainScreen = () => {
+  const preloader = document.querySelector('.preloader');
+  const btn = preloader.querySelector('.preloader__btn');
+  const letter = preloader.querySelector('.preloader__letter');
+  const link = preloader.querySelector('.preloader__loader');
+  const main = document.querySelector('.main');
+  const headerLink = document.querySelector('.header__logo');
+  const mainBlock = main.querySelector('.main__block');
+  const color = document.querySelector('.main__color-block');
+
+  const setAnimation = () => {
+    preloader.classList.add('done');
+    letter.classList.add('done');
+    setTimeout(setInnactive, 3000);
+    setTimeout(setActiveClass, 1000);
+  };
+
+  const setInnactive = () => {
+    preloader.style.display = 'none';
+    main.classList.add('is-active');
+  };
+
+  const setActiveClass = () => {
+    main.classList.add('is-active');
+  };
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      setAnimation();
+    }
+  });
+
+  btn.addEventListener('click', () => {
+    setAnimation();
+  });
+
+  if (window.screen.width < 1024) {
+    window.addEventListener('click', (e) => {
+      if (e.target !== link) {
+        setAnimation();
+      }
+    });
+  }
+
+  window.addEventListener('click', (e) => {
+    if (window.screen.width < 1024 && main.classList.contains('is-active') && e.target !== headerLink) {
+      color.classList.toggle('main__color-block--background-color');
+    }
+  });
+};
+
+export {initMainScreen};
